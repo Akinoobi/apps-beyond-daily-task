2d5b41b0d82af0fe63aa3772a70e53100e0754da
gel-cebu <apalma.halcyondigital@gmail.com>
2023-01-19 17:17:55 +0800

    fix: minor changes & save
    Signed-off-by: gel-cebu <apalma.halcyondigital@gmail.com>

--- src/pages/components/HeaderNavigation.js
+++ src/pages/components/HeaderNavigation.js
@@ -21,6 +21,9 @@ const HeaderNavigation = () => {
         state.theme,
 
     ], shallow)
+    const onFinish = (values) => {
+        console.log("values", values)
+    }
     console.log("title", title, "length", length, "theme", theme)
     return (
         <div
@@ -38,16 +41,19 @@ const HeaderNavigation = () => {
                 headerClass={"text-black mx-auto"}
                 subtitle={"You can edit your task here."}
             >
-                <div className="flex flex-col w-full h-full max-h-[800px] min-h-[500px] max-w-[500px] min-w-[500px] justify-center items-center gap-4">
+                <form
+                onFinish={onFinish}>
+
+                <div className="flex flex-col w-full h-full max-h-[300px] min-h-[300px] max-w-[500px] min-w-[500px] justify-center items-center gap-4">
                     <input type="text"
                         name="title"
                         placeholder="Title"
                         value={title}
-                        className={"placeholder-gray-500  text-black  border-2 border-gray-700 rounded-md"}
+                        className={"placeholder-gray-500  text-black  border-2 border-gray-700 rounded-md p-2"}
                         onChange={(e) => {
                             DailyTaskStateManager.setState({
                                 title: e.target.value
-                              })
+                            })
                         }}
                     />
                     <input
@@ -55,11 +61,11 @@ const HeaderNavigation = () => {
                         name="length"
                         placeholder="Length"
                         value={length}
-                        className={"placeholder-gray-500 border-2 text-black border-gray-700 rounded-md"}
+                        className={"placeholder-gray-500 border-2 text-black border-gray-700 rounded-md p-2"}
                         onChange={(e) => {
                             DailyTaskStateManager.setState({
                                 length: e.target.value
-                              })
+                            })
                         }}
                     />
                     <input
@@ -67,14 +73,23 @@ const HeaderNavigation = () => {
                         name="length"
                         placeholder="Length"
                         value={theme}
-                        className={"placeholder-gray-500 text-black border-2 border-gray-700 rounded-md"}
+                        className={"placeholder-gray-500 text-black border-2 border-gray-700 rounded-md p-2"}
                         onChange={(e) => {
                             DailyTaskStateManager.setState({
                                 theme: e.target.value
-                              })
+                            })
                         }}
                     />
+                    <div className="flex flex-row space-x-4">
+                        <button className="bg-transparent text-gray-700 px-5 py-2 rounded-md" onClick={(e) => setOpenModal(false)}>
+                            Cancel
+                        </button>
+                        <button className="bg-blue-400 text-gray-700 px-5 py-2 rounded-md"   htmlType='submit'>
+                            Save
+                        </button>
+                    </div>
                 </div>
+                </form>
             </Modal>
             <div className='w-full flex  justify-end p-4' onClick={() => setOpenModal(true)}>
                 <BsPlusLg size={30} color="blue" />
